1deb22df81f3a04eccf2b193c74e2615d153edec
Michael Dally <mddally@gmail.com>
2022-04-11 13:18:19 -0400

    commit tree 8e7e9a968f854f6e7be2efa3f1f354c06fbed737

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,52 @@
 # your code here
+class MySet
+    
+    def initialize(enum=[])
+        @hash = {}
+        enum.each do |value|
+            @hash[value] = true
+        end
+    end
+
+    def include?(value)
+        @hash.has_key?(value)
+    end
+
+    def add(value)
+        @hash[value] = true
+        self
+    end
+
+    def delete(value)
+        @hash.delete(value)
+        self
+    end
+
+    def self.[](value)
+        puts value
+        self.initialize(value)
+    end
+
+    def size
+        @hash.length
+    end
+
+    def self.[](*args)
+        new(args)
+    end
+
+    def clear
+        @hash.clear
+        self
+    end
+
+    def each(&block)
+        @hash.keys.each(&block)
+        self
+    end
+
+    def inspect
+        "#<#{self.class.name}: {#{@hash.keys.join(', ')}}>"
+    end
+
+end
